28eb83f38ca9e58bb8eb96f3ca4ec00241fb5891
pabloaguilarm <paguilar@heytrade.com>
2023-10-15 19:12:30 +0200

    refactor: extracting method to arrow function

--- csv-filter/typescript/src/core/csvFilter.ts
+++ csv-filter/typescript/src/core/csvFilter.ts
@@ -8,28 +8,26 @@ export class CsvFilter {
 	get filteredLines(): string[] {
 		const header = this.lines[0];
 		const invoices = this.lines.slice(1);
-		return [header].concat(
-			invoices.filter((invoice) => {
-				const fields = invoice.split(',');
-				const vatField = fields[4];
-				const igicField = fields[5];
-				const decimalRegex = '\\d+(\\.\\d+)?';
-				const taxFieldsAreMutuallyExclusive =
-					(vatField.match(decimalRegex) || igicField.match(decimalRegex)) && (!vatField || !igicField);
-				const netAmountField = fields[3];
-				const grossAmountField = fields[2];
-				const netAmountIsWellCalculated =
-					this.checkIfNetAmountIsCorrect(netAmountField, grossAmountField, vatField) ||
-					this.checkIfNetAmountIsCorrect(netAmountField, grossAmountField, igicField);
-				const cifField = fields[7];
-				const nifField = fields[8];
-				const identificationNumberFieldsAreMutuallyExclusive = (!cifField || !nifField) && !(!cifField && !nifField);
-				return (
-					taxFieldsAreMutuallyExclusive && netAmountIsWellCalculated && identificationNumberFieldsAreMutuallyExclusive
-				);
-			})
-		);
+		return [header].concat(invoices.filter(this.validateInvoice));
 	}
+
+	private validateInvoice = (invoice: string) => {
+		const fields = invoice.split(',');
+		const vatField = fields[4];
+		const igicField = fields[5];
+		const decimalRegex = '\\d+(\\.\\d+)?';
+		const taxFieldsAreMutuallyExclusive =
+			(vatField.match(decimalRegex) || igicField.match(decimalRegex)) && (!vatField || !igicField);
+		const netAmountField = fields[3];
+		const grossAmountField = fields[2];
+		const netAmountIsWellCalculated =
+			this.checkIfNetAmountIsCorrect(netAmountField, grossAmountField, vatField) ||
+			this.checkIfNetAmountIsCorrect(netAmountField, grossAmountField, igicField);
+		const cifField = fields[7];
+		const nifField = fields[8];
+		const identificationNumberFieldsAreMutuallyExclusive = (!cifField || !nifField) && !(!cifField && !nifField);
+		return taxFieldsAreMutuallyExclusive && netAmountIsWellCalculated && identificationNumberFieldsAreMutuallyExclusive;
+	};
 
 	private checkIfNetAmountIsCorrect(netAmountField: string, grossAmountField: string, taxField: string): boolean {
 		const parsedNetAmount = parseFloat(netAmountField);
