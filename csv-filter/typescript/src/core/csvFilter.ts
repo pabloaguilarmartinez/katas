c4a916edcc534338811ef4a9d5d346448ae9f1d3
pabloaguilarm <paguilar@heytrade.com>
2023-10-15 16:25:44 +0200

    Defining examples

---
+++ csv-filter/typescript/src/core/csvFilter.ts
@@ -0,0 +1,9 @@
+/* Examples to test
+ 1. A file with a single invoice where everything is correct should output the same line.
+ 2. A file with a single invoice where VAT and IGIC are filled in, the line should be deleted.
+ 3. A file with a single invoice where the net is incorrectly calculated, should be deleted.
+ 4. A file with a single invoice where VAT and VAT number are filled in, the line should be deleted.
+ 5. If the invoice number is repeated in several lines, all of them are deleted (without leaving any line).
+ 6. An empty list will produce an empty output list.
+ 7. A single line file is incorrect because it has no header.
+* */
