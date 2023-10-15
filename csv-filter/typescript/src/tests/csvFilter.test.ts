b65e8469f79fccf83152a28ce1fe2a55a1418c84
pabloaguilarm <paguilar@heytrade.com>
2023-10-15 17:15:54 +0200

    red: excludes lines with both tax fields populated even if non decimal

--- csv-filter/typescript/src/tests/csvFilter.test.ts
+++ csv-filter/typescript/src/tests/csvFilter.test.ts
@@ -47,4 +47,13 @@ describe('CSV Filter', () => {
 
 		expect(result).toEqual([header]);
 	});
+
+	it('excludes lines with both tax fields populated even if non decimal', () => {
+		const invoiceLine = '1,02/05/2021,1000,790,XYZ,7,ACER Laptop,B76430134,';
+		const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+		const result = csvFilter.filteredLines;
+
+		expect(result).toEqual([header]);
+	});
 });
