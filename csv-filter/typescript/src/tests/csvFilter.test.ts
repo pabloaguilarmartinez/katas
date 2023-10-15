0cd9d851e3ef76fb25a9ab16b50d2b0919b5c1b4
pabloaguilarm <paguilar@heytrade.com>
2023-10-15 17:12:59 +0200

    red: excludes lines with non decimal tax fields

--- csv-filter/typescript/src/tests/csvFilter.test.ts
+++ csv-filter/typescript/src/tests/csvFilter.test.ts
@@ -38,4 +38,13 @@ describe('CSV Filter', () => {
 
 		expect(result).toEqual([header]);
 	});
+
+	it('excludes lines with non decimal tax fields', () => {
+		const invoiceLine = '1,02/05/2021,1000,790,XYZ,,ACER Laptop,B76430134,';
+		const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+		const result = csvFilter.filteredLines;
+
+		expect(result).toEqual([header]);
+	});
 });
