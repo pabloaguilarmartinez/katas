fa39b6571ade01d4c5b9876aa3e6e632cc18cfdb
pabloaguilarm <paguilar@heytrade.com>
2023-10-15 16:48:57 +0200

    red: excludes lines with both tax fields populated as they are exclusive

--- csv-filter/typescript/src/tests/csvFilter.test.ts
+++ csv-filter/typescript/src/tests/csvFilter.test.ts
@@ -19,4 +19,14 @@ describe('CSV Filter', () => {
 
 		expect(result).toEqual([header, invoiceLine]);
 	});
+
+	it('excludes lines with both tax fields populated as they are exclusive', () => {
+		const header = 'Num_invoice, Date, Gross, Net, VAT, IGIC, Concept, CIF_customer, NIF_customer';
+		const invoiceLine = '1,02/05/2021,1000,790,21,7,ACER Laptop,B76430134,';
+		const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+		const result = csvFilter.filteredLines;
+
+		expect(result).toEqual(header);
+	});
 });
