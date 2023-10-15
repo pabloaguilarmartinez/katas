2951623b0ca7ae3dc90baeb8b77fb50b70862874
pabloaguilarm <paguilar@heytrade.com>
2023-10-15 17:01:38 +0200

    red: excludes lines without tax fields populated as one is required

--- csv-filter/typescript/src/tests/csvFilter.test.ts
+++ csv-filter/typescript/src/tests/csvFilter.test.ts
@@ -29,4 +29,13 @@ describe('CSV Filter', () => {
 
 		expect(result).toEqual([header]);
 	});
+
+	it('excludes lines without tax fields populated as one is required', () => {
+		const invoiceLine = '1,02/05/2021,1000,790,,,ACER Laptop,B76430134,';
+		const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+		const result = csvFilter.filteredLines;
+
+		expect(result).toEqual([header]);
+	});
 });
