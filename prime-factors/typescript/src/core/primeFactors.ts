86447cec0564ae8de10bb7fff8bf2ad8bc3d2c8b
pabloaguilarm <paguilar@heytrade.com>
2023-10-22 18:43:06 +0200

    refactor: using ternary

--- prime-factors/typescript/src/core/primeFactors.ts
+++ prime-factors/typescript/src/core/primeFactors.ts
@@ -2,8 +2,5 @@ export function getPrimeFactorsFor(number: number): number[] {
 	let factor = 2;
 	while (number % factor != 0) ++factor;
 	const remainder = number / factor;
-	if (remainder <= 1) {
-		return [factor];
-	}
-	return [factor].concat(getPrimeFactorsFor(remainder));
+	return remainder <= 1 ? [factor] : [factor].concat(getPrimeFactorsFor(remainder));
 }
