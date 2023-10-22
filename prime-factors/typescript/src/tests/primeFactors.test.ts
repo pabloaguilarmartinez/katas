1d62dbe6a30c1098f8c76a24e75bba203e4eac83
pabloaguilarm <paguilar@heytrade.com>
2023-10-22 18:30:28 +0200

    red: finds the prime composition of the given number

--- prime-factors/typescript/src/tests/primeFactors.test.ts
+++ prime-factors/typescript/src/tests/primeFactors.test.ts
@@ -27,5 +27,6 @@ describe('Prime factors', () => {
 		expect(getPrimeFactorsFor(3)).toEqual([3]);
 		expect(getPrimeFactorsFor(3 * 3)).toEqual([3, 3]);
 		expect(getPrimeFactorsFor(2 * 3)).toEqual([2, 3]);
+		expect(getPrimeFactorsFor(5 * 5)).toEqual([5, 5]);
 	});
 });
