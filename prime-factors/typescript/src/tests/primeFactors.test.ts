5adad8b261c6f1ec320dcc7117abb494f2287a0b
pabloaguilarm <paguilar@heytrade.com>
2023-10-22 18:34:45 +0200

    refactor: moving function to another file

--- prime-factors/typescript/src/tests/primeFactors.test.ts
+++ prime-factors/typescript/src/tests/primeFactors.test.ts
@@ -1,23 +1,4 @@
-/*
-2 ⇒ [2]
-2 * 2 ⇒ [2,2]
-2 * 2 * 2 ⇒ [2,2,2]
-3 ⇒ [3]
-3 * 3 ⇒ [3,3]
-3 * 2 ⇒ [2,3]
-5 * 5 ⇒ [5,5]
-5 * 7 * 11 * 3 ⇒ [3,5,7,11]
- */
-function getPrimeFactorsFor(number: number): number[] {
-	let factor = 2;
-	while (number % factor != 0) ++factor;
-	const factors = [factor];
-	const remainder = number / factor;
-	if (remainder > 1) {
-		return factors.concat(getPrimeFactorsFor(remainder));
-	}
-	return factors;
-}
+import { getPrimeFactorsFor } from '../core/primeFactors';
 
 describe('Prime factors', () => {
 	it('finds the prime composition of the given number', () => {
