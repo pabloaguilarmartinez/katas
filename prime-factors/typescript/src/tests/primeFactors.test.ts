d0780704b7c25c7f62620431ade05c32f8385dcc
pabloaguilarm <paguilar@heytrade.com>
2023-10-22 18:39:07 +0200

    refactor: splitting tests to explain better the behaviour

--- prime-factors/typescript/src/tests/primeFactors.test.ts
+++ prime-factors/typescript/src/tests/primeFactors.test.ts
@@ -1,14 +1,16 @@
 import { getPrimeFactorsFor } from '../core/primeFactors';
 
 describe('Prime factors', () => {
-	it('finds the prime composition of the given number', () => {
+	it('knows what is a prime number', () => {
 		expect(getPrimeFactorsFor(2)).toEqual([2]);
-		expect(getPrimeFactorsFor(2 * 2)).toEqual([2, 2]);
-		expect(getPrimeFactorsFor(2 * 2 * 2)).toEqual([2, 2, 2]);
 		expect(getPrimeFactorsFor(3)).toEqual([3]);
+	});
+	it('produces the same result to multiply the numbers in the output list', () => {
+		expect(getPrimeFactorsFor(2 * 2 * 2)).toEqual([2, 2, 2]);
 		expect(getPrimeFactorsFor(3 * 3)).toEqual([3, 3]);
-		expect(getPrimeFactorsFor(2 * 3)).toEqual([2, 3]);
 		expect(getPrimeFactorsFor(5 * 5)).toEqual([5, 5]);
+	});
+	it('orders the prime factors from the smallest to the biggest', () => {
 		expect(getPrimeFactorsFor(5 * 7 * 11 * 3)).toEqual([3, 5, 7, 11]);
 	});
 });
