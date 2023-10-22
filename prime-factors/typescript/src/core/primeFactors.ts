dddd1b47e877e83e6e6616d31c3db3e9b275a75c
pabloaguilarm <paguilar@heytrade.com>
2023-10-22 18:40:24 +0200

    refactor: factors as inline variable since it is not useful

--- prime-factors/typescript/src/core/primeFactors.ts
+++ prime-factors/typescript/src/core/primeFactors.ts
@@ -1,10 +1,9 @@
 export function getPrimeFactorsFor(number: number): number[] {
 	let factor = 2;
 	while (number % factor != 0) ++factor;
-	const factors = [factor];
 	const remainder = number / factor;
 	if (remainder > 1) {
-		return factors.concat(getPrimeFactorsFor(remainder));
+		return [factor].concat(getPrimeFactorsFor(remainder));
 	}
-	return factors;
+	return [factor];
 }
