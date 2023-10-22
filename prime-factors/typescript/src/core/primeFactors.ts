5bdd9f8617319682ffeea51889d60e99ee2dcd63
pabloaguilarm <paguilar@heytrade.com>
2023-10-22 18:53:27 +0200

    refactor: formatting methods and adding return cast

--- prime-factors/typescript/src/core/primeFactors.ts
+++ prime-factors/typescript/src/core/primeFactors.ts
@@ -1,4 +1,9 @@
-function findSmallestPrime(number: number) {
+export function getPrimeFactorsFor(number: number): number[] {
+	checkForPositiveNumber(number);
+	return primeFactors(number);
+}
+
+function findSmallestPrime(number: number): number {
 	if (number === 1) return 1;
 	let factor = 2;
 	while (number % factor != 0) ++factor;
@@ -9,13 +14,8 @@ function checkForPositiveNumber(number: number) {
 	if (number < 1) throw new Error('Only positive numbers are allowed');
 }
 
-function primeFactors(number: number) {
+function primeFactors(number: number): number[] {
 	const prime = findSmallestPrime(number);
 	const remainder = number / prime;
 	return remainder <= 1 ? [prime] : [prime].concat(getPrimeFactorsFor(remainder));
-}
-
-export function getPrimeFactorsFor(number: number): number[] {
-	checkForPositiveNumber(number);
-	return primeFactors(number);
 }
