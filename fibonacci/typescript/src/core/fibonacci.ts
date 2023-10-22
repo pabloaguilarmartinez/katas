ffd124cf9480dee5ac316005e507ad4927a7fd1a
pabloaguilarm <paguilar@heytrade.com>
2023-10-22 17:35:46 +0200

    refactor: modifying second if

--- fibonacci/typescript/src/core/fibonacci.ts
+++ fibonacci/typescript/src/core/fibonacci.ts
@@ -1,5 +1,5 @@
 export function fibonacci(number: number): number {
 	if (number === 0) return 0;
-	if (number <= 2) return 1;
+	if (number === 1) return 1;
 	return fibonacci(number - 1) + fibonacci(number - 2);
 }
