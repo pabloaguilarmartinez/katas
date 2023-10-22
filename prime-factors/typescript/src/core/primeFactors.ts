3eaf5411dc7c9ab69d632b924ae78fb0761969f2
pabloaguilarm <paguilar@heytrade.com>
2023-10-22 18:41:53 +0200

    refactor: setting stop condition before recursion

--- prime-factors/typescript/src/core/primeFactors.ts
+++ prime-factors/typescript/src/core/primeFactors.ts
@@ -2,8 +2,8 @@ export function getPrimeFactorsFor(number: number): number[] {
 	let factor = 2;
 	while (number % factor != 0) ++factor;
 	const remainder = number / factor;
-	if (remainder > 1) {
-		return [factor].concat(getPrimeFactorsFor(remainder));
+	if (remainder <= 1) {
+		return [factor];
 	}
-	return [factor];
+	return [factor].concat(getPrimeFactorsFor(remainder));
 }
