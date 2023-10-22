6cfddc1f0efe0161a6b11d97c936c01034dc35dd
pabloaguilarm <paguilar@heytrade.com>
2023-10-22 17:23:06 +0200

    red: yields value zero to number zero

---
+++ fibonacci/typescript/src/tests/fibonacci.test.ts
@@ -0,0 +1,7 @@
+// 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597
+
+describe('The fibonacci sequence', () => {
+	it('yields value zero to number zero', () => {
+		expect(fibonacci(0)).toBe(0);
+	});
+});
