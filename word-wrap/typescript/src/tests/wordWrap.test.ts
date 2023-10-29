4bee3e2320e94ee1743294d190725f05a1e5f65b
pabloaguilarm <paguilar@heytrade.com>
2023-10-29 12:17:58 +0100

    green: do not add a line break if text is as long as the column width

--- word-wrap/typescript/src/tests/wordWrap.test.ts
+++ word-wrap/typescript/src/tests/wordWrap.test.ts
@@ -4,4 +4,7 @@ describe('The Word Wrapper', () => {
 	it('do not add a line break if text is shorter than column width', () => {
 		expect(wordWrap('', 5)).toBe('');
 	});
+	it('do not add a line break if text is as long as the column width', () => {
+		expect(wordWrap('hello', 5)).toBe('hello');
+	});
 });
