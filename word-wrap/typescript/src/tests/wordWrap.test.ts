c1f9d022b3f69565011541bc0a09cb173f2be89e
pabloaguilarm <paguilar@heytrade.com>
2023-10-29 12:31:53 +0100

    red: part words that are longer than column width

--- word-wrap/typescript/src/tests/wordWrap.test.ts
+++ word-wrap/typescript/src/tests/wordWrap.test.ts
@@ -1,10 +1,13 @@
 import { wordWrap } from '../core/wordWrap';
 
 describe('The Word Wrapper', () => {
-	it('do not add a line break if text is shorter than column width', () => {
+	it('does not add a line break if text is shorter than column width', () => {
 		expect(wordWrap('', 5)).toBe('');
 	});
-	it('do not add a line break if text is as long as the column width', () => {
+	it('does not add a line break if text is as long as the column width', () => {
 		expect(wordWrap('hello', 5)).toBe('hello');
+	});
+	it('part words that are longer than column width', () => {
+		expect(wordWrap('longword', 5)).toBe('long\nword');
 	});
 });
