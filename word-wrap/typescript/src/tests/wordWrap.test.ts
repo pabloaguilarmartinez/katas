e1e8b1888b2d505b07d86a7cab4f6e7aa2318f5c
pabloaguilarm <paguilar@heytrade.com>
2023-10-29 12:14:50 +0100

    green: do not add a line break if text is shorter than column width

--- word-wrap/typescript/src/tests/wordWrap.test.ts
+++ word-wrap/typescript/src/tests/wordWrap.test.ts
@@ -1,3 +1,7 @@
+function wordWrap(text: string, columnWidth: number): string {
+	return text;
+}
+
 describe('The Word Wrapper', () => {
 	it('do not add a line break if text is shorter than column width', () => {
 		expect(wordWrap('', 5)).toBe('');
