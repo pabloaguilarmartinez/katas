470d633bb17f9d3561f5532833af6a4716d133c9
pabloaguilarm <paguilar@heytrade.com>
2023-10-31 08:53:26 +0100

    green: converts the blank space into a break line

--- word-wrap/typescript/src/core/wordWrap.ts
+++ word-wrap/typescript/src/core/wordWrap.ts
@@ -4,7 +4,14 @@ export function wordWrap(text: string, columnWidth: number): string {
 	if (text.length <= columnWidth) {
 		return text;
 	}
-	const wrappedText = text.substring(0, columnWidth).replace(' ', '').concat('\n');
-	const unwrappedText = text.substring(columnWidth);
+	let wrappedText: string;
+	let unwrappedText: string;
+	if (text.indexOf(' ') > -1 && text.indexOf(' ') < columnWidth) {
+		wrappedText = text.substring(0, text.indexOf(' ')).concat('\n');
+		unwrappedText = text.substring(text.indexOf(' ') + 1);
+	} else {
+		wrappedText = text.substring(0, columnWidth).concat('\n');
+		unwrappedText = text.substring(columnWidth);
+	}
 	return wrappedText.concat(wordWrap(unwrappedText, columnWidth));
 }
