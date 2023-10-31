9067163102366317dd6f983a62b060c43d409de0
pabloaguilarm <paguilar@heytrade.com>
2023-10-31 08:48:31 +0100

    refactor: using concat instead of addition

--- word-wrap/typescript/src/core/wordWrap.ts
+++ word-wrap/typescript/src/core/wordWrap.ts
@@ -4,7 +4,7 @@ export function wordWrap(text: string, columnWidth: number): string {
 	if (text.length <= columnWidth) {
 		return text;
 	}
-	const wrappedText = text.substring(0, columnWidth).replace(' ', '') + '\n';
+	const wrappedText = text.substring(0, columnWidth).replace(' ', '').concat('\n');
 	const unwrappedText = text.substring(columnWidth);
 	return wrappedText.concat(wordWrap(unwrappedText, columnWidth));
 }
