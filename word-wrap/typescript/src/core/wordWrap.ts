d48851d97b5887ebe0313e761929150393e23645
pabloaguilarm <paguilar@heytrade.com>
2023-10-31 09:03:42 +0100

    refactor: extracting method

--- word-wrap/typescript/src/core/wordWrap.ts
+++ word-wrap/typescript/src/core/wordWrap.ts
@@ -4,12 +4,21 @@ export function wordWrap(text: string, columnWidth: number): string {
 	if (text.length <= columnWidth) {
 		return text;
 	}
-
-	const indexOfSpace = text.indexOf(' ');
-	const shallWrapBySpace = indexOfSpace > -1 && indexOfSpace < columnWidth;
-	const wrapIndex = shallWrapBySpace ? indexOfSpace : columnWidth;
-	const unwrapIndex = shallWrapBySpace ? indexOfSpace + 1 : columnWidth;
+	const wrapIndex = getWrapIndex(text, columnWidth);
+	const unwrapIndex = getUnwrapIndex(text, columnWidth);
 	const wrappedText = text.substring(0, wrapIndex).concat('\n');
 	const unwrappedText = text.substring(unwrapIndex);
 	return wrappedText.concat(wordWrap(unwrappedText, columnWidth));
+}
+
+function getWrapIndex(text: string, columnWidth: number) {
+	const indexOfSpace = text.indexOf(' ');
+	const shallWrapBySpace = indexOfSpace > -1 && indexOfSpace < columnWidth;
+	return shallWrapBySpace ? indexOfSpace : columnWidth;
+}
+
+function getUnwrapIndex(text: string, columnWidth: number) {
+	const indexOfSpace = text.indexOf(' ');
+	const shallWrapBySpace = indexOfSpace > -1 && indexOfSpace < columnWidth;
+	return shallWrapBySpace ? indexOfSpace + 1 : columnWidth;
 }
