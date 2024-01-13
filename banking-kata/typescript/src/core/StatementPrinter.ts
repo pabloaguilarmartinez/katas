68510cc447cc11cacd720ec291098201f91c8631
pabloaguilarm <paguilar@heytrade.com>
2024-01-13 19:33:26 +0100

    refactor: header to variable

--- banking-kata/typescript/src/core/StatementPrinter.ts
+++ banking-kata/typescript/src/core/StatementPrinter.ts
@@ -2,9 +2,11 @@ import { Transaction } from './Transaction';
 import { Console } from './Console';
 
 export class StatementPrinter {
-	constructor(private readonly console: Console) {
-	}
+	constructor(private readonly console: Console) {}
+
+	private readonly header = 'Date | Amount | Balance';
+
 	print(transactions: Transaction[]): void {
-		this.console.log('Date | Amount | Balance');
+		this.console.log(this.header);
 	}
 }
