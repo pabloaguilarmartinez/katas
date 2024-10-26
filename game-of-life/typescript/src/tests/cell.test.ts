28440f4838a0423aaacadf9ac74cc0a60d81a037
pabloaguilarm <pablooagm@gmail.com>
2024-10-26 10:29:52 +0200

    [game-of-life] red: any live cell with fewer than two live neighbours dies, as if caused by underpopulation

---
+++ game-of-life/typescript/src/tests/cell.test.ts
@@ -0,0 +1,23 @@
+/*
+1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+2. Any live cell with more than three live neighbours dies, as if by overcrowding.
+3. Any live cell with two or three live neighbours lives on to the next generation.
+4. Any dead cell with exactly three live neighbours becomes a live cell.
+ */
+describe('In the game of life', () => {
+  it('any live cell with fewer than two live neighbours dies, as if caused by underpopulation', () => {
+    const numberOfNeighbours = 1;
+    expect(new Cell('ALIVE').regenerate(numberOfNeighbours)).toBe('DEAD');
+    expect(new Cell('DEAD').regenerate(numberOfNeighbours)).toBe('DEAD');
+  });
+});
+
+type CellStatus = 'DEAD' | 'ALIVE';
+
+class Cell {
+  constructor(readonly status: CellStatus) {}
+
+  regenerate(numberOfNeighbours: number): CellStatus {
+    return 'ALIVE';
+  }
+}
