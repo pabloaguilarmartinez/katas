9c4d58b14915868d75a071b7a855401ef6f64644
pabloaguilarmartinez <pablooagm@gmail.com>
2025-04-06 10:27:39 +0200

    [template-engine] red: does not parse template if it is null

--- template-engine/typescript/src/tests/templateEngine.test.ts
+++ template-engine/typescript/src/tests/templateEngine.test.ts
@@ -13,42 +13,50 @@ Edge cases:
 import { parseTemplate } from '../core/templateEngine';
 
 describe('The Template Engine', () => {
-  it('parses template without variables', () => {
-    const templateText = 'This is a template with zero variables';
-    const variables = {};
+  describe('Happy paths', () => {
+    it('parses template without variables', () => {
+      const templateText = 'This is a template with zero variables';
+      const variables = {};
 
-    const result = parseTemplate(templateText, variables);
+      const result = parseTemplate(templateText, variables);
 
-    expect(result).toBe(templateText);
+      expect(result).toBe(templateText);
+    });
+
+    it('parses template with one variable', () => {
+      const templateText = 'This is a template with a ${variable}';
+      const variables = { variable: 'foo' };
+      const expectedResult = 'This is a template with a foo';
+
+      const actualResult = parseTemplate(templateText, variables);
+
+      expect(actualResult).toBe(expectedResult);
+    });
+
+    it('parses template with two variables', () => {
+      const templateText = 'This is a template with a ${variable} and ${anotherVariable}';
+      const variables = { variable: 'foo', anotherVariable: 'bar' };
+      const expectedResult = 'This is a template with a foo and bar';
+
+      const actualResult = parseTemplate(templateText, variables);
+
+      expect(actualResult).toBe(expectedResult);
+    });
+
+    it('parses template with repeated variables', () => {
+      const templateText = 'This is a template with a ${variable} and ${variable}';
+      const variables = { variable: 'foo' };
+      const expectedResult = 'This is a template with a foo and foo';
+
+      const actualResult = parseTemplate(templateText, variables);
+
+      expect(actualResult).toBe(expectedResult);
+    });
   });
 
-  it('parses template with one variable', () => {
-    const templateText = 'This is a template with a ${variable}';
-    const variables = { variable: 'foo' };
-    const expectedResult = 'This is a template with a foo';
-
-    const actualResult = parseTemplate(templateText, variables);
-
-    expect(actualResult).toBe(expectedResult);
-  });
-
-  it('parses template with two variables', () => {
-    const templateText = 'This is a template with a ${variable} and ${anotherVariable}';
-    const variables = { variable: 'foo', anotherVariable: 'bar' };
-    const expectedResult = 'This is a template with a foo and bar';
-
-    const actualResult = parseTemplate(templateText, variables);
-
-    expect(actualResult).toBe(expectedResult);
-  });
-
-  it('parses template with repeated variables', () => {
-    const templateText = 'This is a template with a ${variable} and ${variable}';
-    const variables = { variable: 'foo' };
-    const expectedResult = 'This is a template with a foo and foo';
-
-    const actualResult = parseTemplate(templateText, variables);
-
-    expect(actualResult).toBe(expectedResult);
+  describe('Edge cases', () => {
+    it('does not parse template if it is null', () => {
+      expect(() => parseTemplate(null, {})).toThrow('Template text is null');
+    });
   });
 });
