e733c6c250add1ca78494f8aa5866ef8cdd2c89f
pabloaguilarmartinez <pablooagm@gmail.com>
2025-04-06 10:43:53 +0200

    [template-engine] red: does not parse template if variables are not being found

--- template-engine/typescript/src/core/templateEngine.ts
+++ template-engine/typescript/src/core/templateEngine.ts
@@ -1,8 +1,34 @@
 export function parseTemplate(templateText: string, variables: { [key: string]: string }) {
+  ensureTemplateTextIsValid(templateText);
+  ensureTemplateTextVariablesAreInTheDictionary(templateText, variables);
+
   let parsedText = templateText;
   for (const key in variables) {
-    const regex = new RegExp(`\\$\\{${key}\\}`, 'g');
-    parsedText = parsedText.replace(regex, variables[key]);
+    parsedText = parsedText.replace(variableRegex(key), variables[key]);
   }
   return parsedText;
+}
+
+function ensureTemplateTextIsValid(templateText: string) {
+  if (!templateText) {
+    throw new MissingTemplateTextError();
+  }
+}
+
+function ensureTemplateTextVariablesAreInTheDictionary(templateText: string, variables: { [key: string]: string }) {}
+
+function variableRegex(variableName: string): RegExp {
+  return new RegExp(`\\$\\{${variableName}\\}`, 'g');
+}
+
+export class MissingTemplateTextError extends Error {
+  constructor() {
+    super('Template text is missing');
+  }
+}
+
+export class MissingValueError extends Error {
+  constructor(variableName: string) {
+    super(`Variable ${variableName} value is missing`);
+  }
 }
